d22d328d3dd3143080e0ac8a5e1abf41ae6be1df
Touseef Elahi <touseefelahi@ymail.com>
2019-12-19 16:42:10 +0300

    Flat window support added in style

---
+++ AttachedProperties/WindowButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace FlatStyle
+{
+    public static class Command
+    {
+        public static ParameterizedDelegateCommand Close = new ParameterizedDelegateCommand((parameter) => CloseWindow(parameter));
+        public static ParameterizedDelegateCommand Maximize = new ParameterizedDelegateCommand((parameter) => MaximizeWindow(parameter));
+
+        public static ParameterizedDelegateCommand Minimize = new ParameterizedDelegateCommand((parameter) => MinimizeWindow(parameter));
+
+        private static void MaximizeWindow(object sender)
+        {
+            Window window = sender as Window;
+            switch (window.WindowState)
+            {
+                case WindowState.Normal:
+                    window.WindowState = WindowState.Maximized;
+                    break;
+
+                case WindowState.Maximized:
+                    window.WindowState = WindowState.Normal;
+                    break;
+            }
+        }
+
+        private static void MinimizeWindow(object sender)
+        {
+            Window window = sender as Window;
+            window.WindowState = WindowState.Minimized;
+        }
+
+        private static void CloseWindow(object sender)
+        {
+            Window window = sender as Window;
+            window.Close();
+        }
+    }
+}
