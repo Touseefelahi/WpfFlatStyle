89f785d76abf1aec1f48fcba2a5e0531e3aff37e
Touseef Elahi <touseefelahi@ymail.com>
2020-06-16 11:23:15 +0300

    Textbox passes the current text as aurgument

--- AttachedProperties/GetSetCommand.cs
+++ AttachedProperties/GetSetCommand.cs
@@ -1,8 +1,16 @@
-﻿namespace FlatStyle
-{
-    public class GetCommand : BaseAttachedProperty<GetCommand, ParameterizedDelegateCommand> { }
+﻿using System.Windows.Input;
 
-    public class SetCommand : BaseAttachedProperty<SetCommand, ParameterizedDelegateCommand> { }
+namespace FlatStyle
+{
+    /// <summary>
+    /// Get command will be invoked whenever Get button is pressed from GetSetField Textbox
+    /// </summary>
+    public class GetCommand : BaseAttachedProperty<GetCommand, ICommand> { }
+
+    /// <summary>
+    /// Set command will be invoked whenever Set button is pressed from GetSetField Textbox
+    /// </summary>
+    public class SetCommand : BaseAttachedProperty<SetCommand, ICommand> { }
 
     public class SetText : BaseAttachedProperty<SetText, string> { }
 
