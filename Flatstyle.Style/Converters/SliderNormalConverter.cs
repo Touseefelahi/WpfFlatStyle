68a7e9ee61a7d15ccfa1d72eaf26ecfccf1056c5
Touseef Elahi <touseefelahi@ymail.com>
2021-08-18 14:45:05 +0300

    Add slider converter

--- Flatstyle.Style/Converters/SliderNormalConverter.cs
+++ Flatstyle.Style/Converters/SliderNormalConverter.cs
@@ -15,20 +15,21 @@ namespace FlatStyle.Converters
             if (stringSplits.Length == 2 && stringSplits[1].Length > 12)
             {
                 int zeroCount = stringSplits[1].Split('0').Length - 1;
-                if (zeroCount > stringSplits[1].Length - 5)
+                double percentage = 0.6;
+                if (zeroCount > stringSplits[1].Length * percentage)
                 {
                     return Math.Round((double)value, 5).ToString();
                 }
 
                 int nineCount = stringSplits[1].Split('9').Length - 1;
-                if (nineCount > stringSplits[1].Length - 5)
+                if (nineCount > stringSplits[1].Length * percentage)
                 {
                     return Math.Round((double)value, 5).ToString();
                 }
                 for (int i = 1; i < 9; i++)
                 {
                     int count = stringSplits[1].Split($"{i}").Length - 1;
-                    if (count > stringSplits[1].Length - 5)
+                    if (count > stringSplits[1].Length * percentage)
                     {
                         return Math.Round((double)value, 3).ToString();
                     }
